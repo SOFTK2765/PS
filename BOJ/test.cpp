9da787e2de38709e188920c7fb9399df959c1944
SOFTK2765 <sohot2765@gmail.com>
2020-03-04 20:45:42 +0900

    20200304

--- BOJ/test.cpp
+++ BOJ/test.cpp
@@ -2,26 +2,9 @@
 
 using namespace std;
 
-int a[4][4];
-
 int main()
 {
-    for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-            a[i][j] = i*3+j;
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-            printf("%d ", a[i][j]);
-        printf("\n");
-    }
-    memset(a[0], 0, sizeof(a[0]));
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-            printf("%d ", a[i][j]);
-        printf("\n");
-    }
+    
 
     return 0;
 }
